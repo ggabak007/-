7d47afd4756a4ac00a970c3448831b9baeb1c589
ggabak007 <soomin426437@gmail.com>
2026-03-13 00:02:44 +0900

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -86,3 +86,82 @@ export const FISH_DATA = [
   { id: 83, name: "황새치", level: 10, location: ["고래바다"], weather: "무지개", time: [6, 12], image: "" },
   { id: 84, name: "청상아리", level: 10, location: ["바다낚시"], weather: "무지개", time: [6, 12], image: "" }
 ];
+
+export interface CookingItem {
+  id: number;
+  name: string;
+  ingredients: string;      
+  ingredientTags: string[]; 
+  price1: string;
+  price2: string;
+  price3: string;
+  price4: string;
+  price5: string;
+  note?: string;            
+}
+
+export const COOKING_DATA: CookingItem[] = [
+  { id: 1, name: "케첩", ingredients: "토마토 ×4", ingredientTags: ["토마토"], price1: "180", price2: "270", price3: "360", price4: "720", price5: "1440" },
+  { id: 2, name: "딸기 잼", ingredients: "딸기 ×4", ingredientTags: ["딸기"], price1: "1580", price2: "2370", price3: "3160", price4: "6320", price5: "12640" },
+  { id: 3, name: "포도 잼", ingredients: "포도 ×4", ingredientTags: ["포도"], price1: "2020", price2: "3030", price3: "4040", price4: "8080", price5: "16160" },
+  
+  // 구운 버섯 시리즈 상세화
+  { id: 4, name: "구운 버섯", ingredients: "버섯 ×4", ingredientTags: ["버섯"], price1: "180", price2: "270", price3: "360", price4: "720", price5: "1440" },
+  { id: 5, name: "구운 느타리버섯", ingredients: "느타리버섯 ×4", ingredientTags: ["느타리버섯"], price1: "180", price2: "270", price3: "360", price4: "720", price5: "1440" },
+  { id: 6, name: "구운 표고버섯", ingredients: "표고버섯 ×4", ingredientTags: ["표고버섯"], price1: "180", price2: "270", price3: "360", price4: "720", price5: "1440" },
+  { id: 7, name: "구운 양송이버섯", ingredients: "양송이버섯 ×4", ingredientTags: ["양송이버섯"], price1: "180", price2: "270", price3: "360", price4: "720", price5: "1440" },
+  { id: 8, name: "구운 그물버섯", ingredients: "그물버섯 ×4", ingredientTags: ["그물버섯"], price1: "180", price2: "270", price3: "360", price4: "720", price5: "1440" },
+
+  // 버섯 파이 시리즈 상세화
+  { id: 9, name: "버섯 파이", ingredients: "버섯×2, 밀×1, 달걀×1", ingredientTags: ["버섯", "밀", "달걀"], price1: "500", price2: "750", price3: "1000", price4: "2000", price5: "4000" },
+  { id: 10, name: "느타리버섯 파이", ingredients: "느타리버섯×2, 밀×1, 달걀×1", ingredientTags: ["느타리버섯", "밀", "달걀"], price1: "500", price2: "750", price3: "1000", price4: "2000", price5: "4000" },
+  { id: 11, name: "표고버섯 파이", ingredients: "표고버섯×2, 밀×1, 달걀×1", ingredientTags: ["표고버섯", "밀", "달걀"], price1: "500", price2: "750", price3: "1000", price4: "2000", price5: "4000" },
+  { id: 12, name: "양송이버섯 파이", ingredients: "양송이버섯×2, 밀×1, 달걀×1", ingredientTags: ["양송이버섯", "밀", "달걀"], price1: "500", price2: "750", price3: "1000", price4: "2000", price5: "4000" },
+  { id: 13, name: "그물버섯 파이", ingredients: "그물버섯×2, 밀×1, 달걀×1", ingredientTags: ["그물버섯", "밀", "달걀"], price1: "500", price2: "750", price3: "1000", price4: "2000", price5: "4000" },
+
+  { id: 14, name: "검은 트러플 파이", ingredients: "트러플×2, 밀×1, 달걀×1", ingredientTags: ["트러플", "밀", "달걀"], price1: "830", price2: "1245", price3: "1660", price4: "3320", price5: "6640", note: "최고 💎" },
+  { id: 15, name: "치즈케이크", ingredients: "치즈×1, 우유×1, 밀×1", ingredientTags: ["치즈", "우유", "밀"], price1: "480", price2: "720", price3: "960", price4: "1920", price5: "3840" },
+  { id: 16, name: "커피", ingredients: "원두 ×4", ingredientTags: ["원두"], price1: "290", price2: "435", price3: "580", price4: "1160", price5: "2320" },
+  { id: 17, name: "카페라떼", ingredients: "원두×2, 우유×2", ingredientTags: ["원두", "우유"], price1: "300", price2: "450", price3: "600", price4: "1200", price5: "2400" },
+  { id: 18, name: "미트버거", ingredients: "밀, 양상추, 고기, 케첩 각 1", ingredientTags: ["밀", "양상추", "고기", "케첩"], price1: "1350", price2: "2025", price3: "2700", price4: "5400", price5: "10800" },
+  { id: 19, name: "콘수프", ingredients: "우유×1, 버터×1, 옥수수×2", ingredientTags: ["우유", "버터", "옥수수"], price1: "1340", price2: "2010", price3: "2680", price4: "5360", price5: "10720" },
+  { id: 20, name: "황금 킹크랩찜", ingredients: "황금 킹크랩×3, 버터×1", ingredientTags: ["황금 킹크랩", "버터"], price1: "2980", price2: "4470", price3: "5960", price4: "11920", price5: "23840" },
+  { id: 21, name: "캠핑세트", ingredients: "완제품 4종 조합", ingredientTags: ["완제품"], price1: "2260", price2: "3390", price3: "4520", price4: "9040", price5: "18080" },
+  
+  // 롤케이크 시리즈 상세화
+  { id: 22, name: "오리지널 롤케이크", ingredients: "흰색사탕×2, 밀×1, 달걀×1", ingredientTags: ["흰색사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 23, name: "레드 롤케이크", ingredients: "빨간사탕×2, 밀×1, 달걀×1", ingredientTags: ["빨간사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 24, name: "오렌지 롤케이크", ingredients: "주황사탕×2, 밀×1, 달걀×1", ingredientTags: ["주황사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 25, name: "옐로우 롤케이크", ingredients: "노란사탕×2, 밀×1, 달걀×1", ingredientTags: ["노란사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 26, name: "퍼플 롤케이크", ingredients: "보라사탕×2, 밀×1, 달걀×1", ingredientTags: ["보라사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 27, name: "그린 롤케이크", ingredients: "초록사탕×2, 밀×1, 달걀×1", ingredientTags: ["초록사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 28, name: "스카이 롤케이크", ingredients: "하늘사탕×2, 밀×1, 달걀×1", ingredientTags: ["하늘사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+  { id: 29, name: "블루 롤케이크", ingredients: "파란사탕×2, 밀×1, 달걀×1", ingredientTags: ["파란사탕", "밀", "달걀"], price1: "550", price2: "855", price3: "1140", price4: "2280", price5: "4560" },
+
+  { id: 30, name: "괴상한 음식", ingredients: "요리 실패 (식사)", ingredientTags: [], price1: "20", price2: "-", price3: "-", price4: "-", price5: "-", note: "실패작" },
+  { id: 31, name: "괴상한 음료", ingredients: "요리 실패 (음료)", ingredientTags: [], price1: "30", price2: "-", price3: "-", price4: "-", price5: "-", note: "실패작" },
+  { id: 32, name: "믹스드 잼", ingredients: "잼 재료 ×4", ingredientTags: ["잼 재료"], price1: "160", price2: "240", price3: "320", price4: "640", price5: "1280" },
+  { id: 33, name: "라즈베리 잼", ingredients: "라즈베리 ×4", ingredientTags: ["라즈베리"], price1: "250", price2: "375", price3: "500", price4: "1000", price5: "2000" },
+  { id: 34, name: "블루베리 잼", ingredients: "블루베리 ×4", ingredientTags: ["블루베리"], price1: "170", price2: "255", price3: "340", price4: "680", price5: "1360" },
+  { id: 35, name: "사과 잼", ingredients: "사과 ×4", ingredientTags: ["사과"], price1: "270", price2: "405", price3: "540", price4: "1080", price5: "2160" },
+  { id: 36, name: "오렌지 잼", ingredients: "오렌지 ×4", ingredientTags: ["오렌지"], price1: "270", price2: "405", price3: "540", price4: "1080", price5: "2160" },
+  { id: 37, name: "파인애플 잼", ingredients: "파인애플 ×4", ingredientTags: ["파인애플"], price1: "280", price2: "420", price3: "560", price4: "1120", price5: "2240" },
+  { id: 38, name: "얼음컵 커피/라떼", ingredients: "슈가파우더, 원두, (우유)", ingredientTags: ["슈가파우더", "원두", "우유"], price1: "280", price2: "420", price3: "560", price4: "1120", price5: "2240" },
+  { id: 39, name: "갈은 무와 스테이크", ingredients: "슈가파우더×1, 고기×2, 무×1", ingredientTags: ["슈가파우더", "고기", "무"], price1: "630", price2: "945", price3: "1260", price4: "2520", price5: "5040" },
+  { id: 40, name: "무 크림 수프", ingredients: "슈가파우더×1, 우유×1, 버터×1, 무×1", ingredientTags: ["슈가파우더", "우유", "버터", "무"], price1: "340", price2: "510", price3: "680", price4: "1360", price5: "2720" },
+  { id: 41, name: "슈가파우더 팬케이크", ingredients: "슈가파우더, 우유, 달걀, 과일", ingredientTags: ["슈가파우더", "우유", "달걀", "과일"], price1: "330~360", price2: "495~540", price3: "660~720", price4: "1320~1440", price5: "2640~2880" },
+  { id: 42, name: "오로라 만찬", ingredients: "팬케이크, 음료, 스테이크, 수프 각 1", ingredientTags: ["팬케이크", "음료", "스테이크", "수프"], price1: "1630", price2: "2445", price3: "3260", price4: "6520", price5: "13040" },
+  { id: 43, name: "베지 샐러드", ingredients: "채소 ×2", ingredientTags: ["채소"], price1: "90", price2: "135", price3: "180", price4: "360", price5: "720" },
+  { id: 44, name: "피시 앤 칩스", ingredients: "물고기×2, 감자×2", ingredientTags: ["물고기", "감자"], price1: "310", price2: "465", price3: "620", price4: "1240", price5: "2480" },
+  { id: 45, name: "훈제 연어 베이글", ingredients: "어류×2, 치즈×1, 토마토×1, 밀×1", ingredientTags: ["어류", "치즈", "토마토", "밀"], price1: "520", price2: "780", price3: "1040", price4: "2080", price5: "4160" },
+  { id: 46, name: "애플파이", ingredients: "사과×1, 밀×1, 달걀×1, 버터×1", ingredientTags: ["사과", "밀", "달걀", "버터"], price1: "730", price2: "1095", price3: "1460", price4: "2920", price5: "5840" },
+  { id: 47, name: "당근 케이크", ingredients: "달걀×1, 당근×2, 밀×1", ingredientTags: ["달걀", "당근", "밀"], price1: "840", price2: "1260", price3: "1680", price4: "3360", price5: "6720" },
+  { id: 48, name: "티라미수", ingredients: "원두×1, 달걀×1, 우유×1, 치즈×1", ingredientTags: ["원두", "달걀", "우유", "치즈"], price1: "530", price2: "795", price3: "1060", price4: "2120", price5: "4240" },
+  { id: 49, name: "캔들라이트 디너", ingredients: "샐러드, 베이글, 덮밥, 티라미수 각 1", ingredientTags: ["샐러드", "베이글", "덮밥", "티라미수"], price1: "1760", price2: "2640", price3: "3520", price4: "7040", price5: "14080" },
+  { id: 50, name: "온천란", ingredients: "멸균란 ×1", ingredientTags: ["멸균란"], price1: "130", price2: "195", price3: "260", price4: "520", price5: "1040" },
+];
+
+// 요리 필터용 전체 재료 목록 (중복 제거, 정렬)
+export const ALL_INGREDIENTS = Array.from(
+  new Set(COOKING_DATA.flatMap(item => item.ingredientTags))
+).filter(t => t.length > 0).sort();
